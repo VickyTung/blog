c9c98045f45d8d0251fdf4b9c339e9a864c069bc
VickyTung <vicky12139@gmail.com>
2018-06-11 21:25:12 -0700

    Logs AOP

--- blog/src/main/java/com/haojing/blog/web/IndexController.java
+++ blog/src/main/java/com/haojing/blog/web/IndexController.java
@@ -3,16 +3,18 @@ package com.haojing.blog.web;
 import com.haojing.blog.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
-    @GetMapping("/")
-    public String index() {
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id, @PathVariable String name) {
 //        int i = 9/0;
-        String blog = null;
-        if (blog == null) {
-            throw new NotFoundException("Blog does not exist");
-        }
+//        String blog = null;
+//        if (blog == null) {
+//            throw new NotFoundException("Blog does not exist");
+//        }
+        System.out.println("-------index-----");
         return "index";
     }
 }

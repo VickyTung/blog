9ce4b634cdddd472bc0d4bda34d09486e4305d73
VickyTung <vicky12139@gmail.com>
2018-06-19 15:35:00 -0700

    blog content

--- blog/src/main/java/com/haojing/blog/dao/BlogRepository.java
+++ blog/src/main/java/com/haojing/blog/dao/BlogRepository.java
@@ -2,6 +2,7 @@ package com.haojing.blog.dao;
 
 import com.haojing.blog.po.Blog;
 import com.haojing.blog.service.BlogService;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -13,5 +14,8 @@ public interface BlogRepository extends JpaRepository<Blog, Long>, JpaSpecificat
 
     @Query("select b from Blog b where b.recommended = true ")
     List<Blog> findTop(Pageable pageable);
+
+    @Query("select b from Blog b where b.title like ?1 or b.content like ?1")
+    Page<Blog> findByQuery(String query, Pageable pageable);
 }
 
